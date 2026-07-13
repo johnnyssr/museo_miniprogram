de85add3fa45d5bfd85d8646ec600012e23ae16a
i354885 <johnny.shi@sap.com>
2026-07-13 15:00:35 +0800

    chore: enable wx cloud and init cloud env

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -2,6 +2,16 @@
 App<IAppOption>({
   globalData: {},
   onLaunch() {
+    // 初始化云开发
+    if (!wx.cloud) {
+      console.error('当前基础库版本过低，无法使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'cloud1-d6gnwyekz0f64654f',
+        traceUser: true,
+      })
+    }
+
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
